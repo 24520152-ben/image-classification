fa8e458e9901a3c25a356c6c762a2ae7bdf2003d
ben <24520152@gm.uit.edu.vn>
2025-11-05 21:19:07 +0700

    Define DenseNet121 in ./src/models/model_builders.py

--- src/models/model_builders.py
+++ src/models/model_builders.py
@@ -27,7 +27,37 @@ def build_efficientnet(INPUT_SHAPE=(224, 224, 3), NUM_CLASSES=6, DROPOUT_RATE=0.
 
     x = backbone.output
     x = layers.Dropout(DROPOUT_RATE, name="head_dropout")(x)
-    output_layer = layers.Dense(NUM_CLASSES, activation='softmax', name="output_layer")(x)
+    output_layer = layers.Dense(NUM_CLASSES, activation="softmax", name="output_layer")(x)
 
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer, name="EfficientNetB0_FeatureExtractor")
+    return model
+
+def build_densenet(INPUT_SHAPE=(224, 224, 3), NUM_CLASSES=6, DROPOUT_RATE=0.3):
+    """
+    Build an DenseNet121 model with a custom head for classification
+    This uses feature extraction (freezing the backbone)
+
+    Args:
+        INPUT_SHAPE (tuple): The shape of the input images
+        NUM_CLASSES (int): The number of output classes
+        DROPOUT_RATE (float): Dropout rate for the classifier head
+
+    Returns:
+        tf.keras.Model: The compiled Keras model
+    """
+    input_layer = tf.keras.Input(shape=INPUT_SHAPE, name="input_layer")
+
+    backbone = applications.DenseNet121(
+        include_top=False,
+        weights="imagenet",
+        input_tensor=input_layer,
+        pooling="avg",
+    )
+    backbone.trainable = False
+
+    x = backbone.output
+    x = layers.Dropout(DROPOUT_RATE, name="head_dropout")(x)
+    output_layer = layers.Dense(NUM_CLASSES, activation="softmax", name="output_layer")(x)
+
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer, name="DenseNet121_FeatureExtractor")
     return model
