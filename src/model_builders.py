83fa975ae99f484edb9dedf061ed498890fe5b86
ben <24520152@gm.uit.edu.vn>
2025-11-06 16:36:04 +0700

    Delete Vision Transformer

--- src/model_builders.py
+++ src/model_builders.py
@@ -60,33 +60,6 @@ def build_densenet(INPUT_SHAPE=(224, 224, 3), NUM_CLASSES=6, DROPOUT_RATE=0.3):
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer, name="DenseNet121")
     return model
 
-def build_visiontransformer(INPUT_SHAPE=(224, 224, 3), NUM_CLASSES=6, DROPOUT_RATE=0.3):
-    """
-    Build an Vision Transformer base model with a custom head for classification
-    This uses feature extraction (freezing the backbone)
-
-    Args:
-        INPUT_SHAPE (tuple): The shape of the input images
-        NUM_CLASSES (int): The number of output classes
-        DROPOUT_RATE (float): Dropout rate for the classifier head
-
-    Returns:
-        tf.keras.Model: The compiled Keras model
-    """
-    input_layer = tf.keras.Input(shape=INPUT_SHAPE, name="input_layer")
-
-    backbone = keras_hub.models.Backbone.from_preset(
-        "vit_base_patch16_224_imagenet"
-    )
-    backbone.trainable = False
-
-    x = backbone(input_layer)
-    x = layers.Dropout(DROPOUT_RATE, name="dropout_layer")(x)
-    output_layer = layers.Dense(NUM_CLASSES, activation="softmax", name="output_layer")(x)
-
-    model = tf.keras.Model(inputs=input_layer, outputs=output_layer, name="VisionTransformer")
-    return model
-
 def build_resnet(INPUT_SHAPE=(224, 224, 3), NUM_CLASSES=6, DROPOUT_RATE=0.3):
     """
     Build an ResNet50V2 model with a custom head for classification
@@ -148,7 +121,6 @@ def build_mobilenet(INPUT_SHAPE=(224, 224, 3), NUM_CLASSES=6, DROPOUT_RATE=0.3):
 MODEL_BUILDERS = {
     "EfficientNetB5": build_efficientnet,
     "DenseNet121": build_densenet,
-    "VisionTransformer": build_visiontransformer,
     "ResNet50V2": build_resnet,
     "MobileNetV2": build_mobilenet,
 }
